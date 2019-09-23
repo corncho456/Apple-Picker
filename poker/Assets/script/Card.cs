7c83648179f4d761df18afe565dceb0ba887e0fc
corncho1007 <corncho1007@gmail.com>
2019-09-23 18:48:13 +0900

    poker 프로젝트

---
+++ poker/Assets/script/Card.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public string suit;
+    public int rank;
+    public GameObject back;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class CardDefinition
+{
+    public string face;
+    public int rank;
+    public List<CardSprite> pips = new List<CardSprite>();
+}
+
+public class CardSprite
+{
+    public Sprite image;
+
+}
